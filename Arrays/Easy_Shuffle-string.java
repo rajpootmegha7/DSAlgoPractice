6698c79ebb6208e8ed662d897752c789cb3ee9fe
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 01:32:43 -0400

    Update and rename Easy_Shuffle-string to Easy_Shuffle-string.java

--- Arrays/Easy_Shuffle-string.java
+++ Arrays/Easy_Shuffle-string.java
@@ -1,4 +1,5 @@
 
+/**
 You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 
 Return the shuffled string.
@@ -6,7 +7,7 @@ Example 1 :
 Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
 Output: "leetcode"
 Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
-
+*/
 class Solution {
     public String restoreString(String s, int[] indices) {
         //Exit condition if the indices and string is same then return the same string.
