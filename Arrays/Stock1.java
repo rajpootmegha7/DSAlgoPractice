c72ffd3251c892e2c26413ec915298cd62421505
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 02:18:07 -0400

    Update Stock1.java

--- Arrays/Stock1.java
+++ Arrays/Stock1.java
@@ -20,10 +20,10 @@ class Solution {
     }
     // O(n) time complexity for this solution.
     public int onePass(int[] prices){
-        int min = prices[0]; 
+        int min = prices[0]; // Assign first val as min
         int profit = 0;
         for(int i = 1; i < prices.length; i++){
-            if(prices[i] > min){
+            if(prices[i] > min){  // compare all the other values to min
                 profit = Math.max(profit, prices[i] - min);
             } else min = prices[i];
             
