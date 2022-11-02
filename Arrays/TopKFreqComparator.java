dce53dd485911fe2d30003d446fd7546055e0640
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 03:12:08 -0400

    Update and rename TopK Frequent HashMap to TopKFreqComparator.java

--- Arrays/TopKFreqComparator.java
+++ Arrays/TopKFreqComparator.java
@@ -1,6 +1,8 @@
+/**
 Input: nums = [1,1,1,2,2,3], k = 2
 Output: [1,2]
 Using Hashmap and comparator solved it.
+*/
 
 class Solution {
     public int[] topKFrequent(int[] nums, int num) {
@@ -19,6 +21,8 @@ class Solution {
         hm.forEach((key,v) -> System.out.println(key+" and "+ v));
         Set<Map.Entry<Integer, Integer>> entrySet = hm.entrySet();
         List<Map.Entry<Integer, Integer>> list = new ArrayList<>(entrySet);
+       
+        // Using collections and overload the comparator interface compare function.
         Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
             @Override
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
@@ -27,9 +31,11 @@ class Solution {
                 else return -1;
             }
         });
-
-        list.forEach(s -> System.out.println(s.getKey()+ "and "+ s.getValue()));
+        // lambda to print hashmap.
+        list.forEach(s -> System.out.println(s.getKey()+ "and "+ s.getValue())); 
+        
         int i =num -1;
+        // convert the list to array.
         while(num-->0){
             Map.Entry<Integer, Integer> en = list.get(i);
             out[i] = en.getKey();
@@ -39,24 +45,28 @@ class Solution {
     }
     
     
-    Solution 2 using priority queue
+    //Solution 2 using priority queue
     
     public int[] topKFrequent(int[] nums, int k) {
         int[] arr = new int[k];
         HashMap<Integer, Integer> map = new HashMap<>();
+        
         for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
         PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
                 (a, b) ->
             a.getValue() - b.getValue()
         );
+        
         for (Map.Entry<Integer, Integer> it : map.entrySet()) {
             pq.add(it);
             if (pq.size() > k) pq.poll();
         }
+        
         int i = k;
         while (!pq.isEmpty()) {
             arr[--i] = pq.poll().getKey();
         }
+        
         return arr;
     }
 }
