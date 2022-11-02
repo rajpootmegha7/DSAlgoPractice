9315b96cb2230fc3631145bd5c0979aa2006fc49
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 02:05:11 -0400

    Update and rename LongestRepeatingCharacterReplacement to LongestRepeatingCharacterReplacement.java

--- Arrays/LongestRepeatingCharacterReplacement.java
+++ Arrays/LongestRepeatingCharacterReplacement.java
@@ -1,3 +1,4 @@
+/**
 You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. 
 You can perform this operation at most k times.
 
@@ -7,14 +8,15 @@ Return the length of the longest substring containing the same letter you can ge
 Input: s = "ABAB", k = 2
 Output: 4
 Explanation: Replace the two 'A's with two 'B's or vice versa.
-
+*/
 
 public int characterReplacement(String s, int k) {
 
-        HashMap<Character, Integer> map = new HashMap<>();
-        int left = 0, right = 0;
-        int max_len = 0;
-        int most_freq = 0;
+        // Taken the map to store the char and occurance of that char.
+        HashMap<Character, Integer> map = new HashMap<>(); 
+        int left = 0, right = 0; // for sliding window pointers [left ....right]
+        int max_len = 0;      // max length of the window
+        int most_freq = 0;  // most freq char in the window
 
         while (right < s.length()) {
             //increase sliding window
@@ -23,13 +25,13 @@ public int characterReplacement(String s, int k) {
             } else {
                 map.put(s.charAt(right), 0);
             }
-            most_freq = Math.max(most_freq, map.get(s.charAt(right)));
+            most_freq = Math.max(most_freq, map.get(s.charAt(right))); // update the most freq.
 
             if ((right - left + 1) - most_freq > k) { // shrink the window
                 map.put(s.charAt(right), map.get(s.charAt(right)) - 1);
                 left++;
             }
-            max_len = Math.max(right - left + 1, max_len);
+            max_len = Math.max(right - left + 1, max_len); // update the max length
             right++;
         }
         return max_len;
