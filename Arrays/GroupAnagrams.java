1288d764e4a1faae272d670a6ca9a6672de7c231
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 03:17:24 -0400

    Update and rename groupAnagrams to GroupAnagrams.java

--- Arrays/GroupAnagrams.java
+++ Arrays/GroupAnagrams.java
@@ -1,9 +1,10 @@
+/**
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 Input: strs = ["eat","tea","tan","ate","nat","bat"]
 Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-
+*/
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
