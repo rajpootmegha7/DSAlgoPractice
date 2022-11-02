a7a29963fcd704b9243a733cabe4af07260da1e9
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 01:29:31 -0400

    Update and rename Easy_MinAbsDiff to Easy_MinAbsDiff.java

--- Arrays/Easy_MinAbsDiff.java
+++ Arrays/Easy_MinAbsDiff.java
@@ -1,3 +1,4 @@
+/**
 Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
 
 Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
@@ -9,8 +10,9 @@ b - a equals to the minimum absolute difference of any two elements in arr
 Example 1:
 Input: arr = [4,2,1,3]
 Output: [[1,2],[2,3],[3,4]]
-Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
 
+Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+*/
 
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
