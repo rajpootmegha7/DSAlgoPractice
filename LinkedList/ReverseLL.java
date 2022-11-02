684bccd1116ed7ea497eb7347506478c627bb8ed
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 00:08:54 -0400

    Create ReverseLL.java

---
+++ LinkedList/ReverseLL.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode curr = head;
+        if(head == null || head.next == null) return head;
+        ListNode prev = null; // 1. Create a node prev and set it to null.
+        while(head != null){
+           
+            ListNode next = head.next; // 2. Create a temporary node next and save the head.next list there.
+            head.next = prev; //3. Point the head.next to prev, to reverse it.
+            prev = head; // 4. Move prev to the head.
+            head = next; // 5. Move head to the next where we saved the remaining list.
+        
+        }
+        
+        return prev; // prev will have the pointer of reversed list.
+    }
+}
