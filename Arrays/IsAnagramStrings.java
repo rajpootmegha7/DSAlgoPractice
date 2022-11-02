4b3391a97f6575cea8e47f8628c1ff479b459fd6
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 01:47:20 -0400

    Update and rename IsAnagramStrings to IsAnagramStrings.java

--- Arrays/IsAnagramStrings.java
+++ Arrays/IsAnagramStrings.java
@@ -1,3 +1,4 @@
+/**
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
@@ -10,7 +11,7 @@ Output: true
 Input: s = "a", t = "ab"
 Output: false
 
-
+*/
 class Solution {
     public boolean isAnagram(String s, String t) {
         char[] s1 = s.toCharArray();
