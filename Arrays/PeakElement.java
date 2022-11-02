9061eb7415df98f0607cf06951ab983804621203
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 03:20:20 -0400

    Update and rename peakElement to PeakElement.java

--- Arrays/PeakElement.java
+++ Arrays/PeakElement.java
@@ -1,4 +1,4 @@
-
+/**
 A peak element is an element that is strictly greater than its neighbors.
 
 Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
@@ -9,7 +9,7 @@ Example 1:
 Input: nums = [1,2,3,1]
 Output: 2
 Explanation: 3 is a peak element and your function should return the index number 2.
-
+*/
 class Solution {
     public int findPeakElement(int[] nums) {
         return iterative(nums);
