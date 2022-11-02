fb33bbdcb5cffbd7c737fc9d23f93fc61fb0c371
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 01:28:47 -0400

    Update EasyPlus.java

--- Arrays/EasyPlus.java
+++ Arrays/EasyPlus.java
@@ -1,3 +1,4 @@
+/**
 You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. 
 The large integer does not contain any leading 0's.
 Increment the large integer by one and return the resulting array of digits.
@@ -10,7 +11,7 @@ Explanation: The array represents the integer 123.
 Incrementing by one gives 123 + 1 = 124.
 Thus, the result should be [1,2,4].
 
-
+*/
 
 class Solution {
     public int[] plusOne(int[] digits) {
