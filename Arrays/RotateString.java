b55333fb64d08eabca77a04026137841caab58fa
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 03:21:33 -0400

    Update and rename rotateString to RotateString.java

--- Arrays/RotateString.java
+++ Arrays/RotateString.java
@@ -1,10 +1,11 @@
+/**
 Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
 
 A shift on s consists of moving the leftmost character of s to the rightmost position.
 
 For example, if s = "abcde", then it will be "bcdea" after one shift.
 
-/**
+
 
 There is very easy trick to solve this if goal string is a substring of the s+s then this means it is true.
 */
@@ -31,7 +32,7 @@ class Solution {
             int m = i;
             // Always remember to check the length first before iterating it further.
             while(m <= str1.length() - 1 && str2.charAt(k) == str1.charAt(m)){
-                // This line is checking if the length of k is matching with the total length then                       // condition is true.
+                // This line is checking if the length of k is matching with the total length then condition is true.
                 if(k == str2.length()-1) return true;
                 k++;
                 m++;
