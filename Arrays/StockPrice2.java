03d4fdbf42619f45485b8e4f8db55d78fb3fc520
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 02:49:09 -0400

    Update and rename StockPrice2 to StockPrice2.java

--- Arrays/StockPrice2.java
+++ Arrays/StockPrice2.java
@@ -1,3 +1,4 @@
+/**
 You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
 However, you can buy it then immediately sell it on the same day.
@@ -10,6 +11,7 @@ Output: 7
 Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
 Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 Total profit is 4 + 3 = 7.
+*/
 
 class Solution {
     public int maxProfit(int[] prices) {
@@ -20,7 +22,7 @@ class Solution {
        int profit = 0;
         for(int i = 0; i < arr.length -1; i++){
            if(arr[i+1] > arr[i]){
-               profit += arr[i+1]- arr[i];
+               profit += arr[i+1]- arr[i]; 
            } 
         }
         return profit;
