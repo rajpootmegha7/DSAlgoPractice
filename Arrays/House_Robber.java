f38b606b33f77136653dc5e88b77b330a4e966ab
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 01:38:53 -0400

    Update and rename House_Robber to House_Robber.java

--- Arrays/House_Robber.java
+++ Arrays/House_Robber.java
@@ -1,3 +1,4 @@
+/**
 You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, 
 the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and 
 it will automatically contact the police if two adjacent houses were broken into on the same night.
@@ -10,7 +11,7 @@ Output: 4
 Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
 Total amount you can rob = 1 + 3 = 4.
 
-
+*/
 
 class Solution {
     public int rob(int[] nums) {
