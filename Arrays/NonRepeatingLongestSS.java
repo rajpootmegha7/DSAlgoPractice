feebeb76b042829b93e9b8d6c6f53d977fdcefc1
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 02:07:05 -0400

    Update and rename NonRepeatingLongestSS to NonRepeatingLongestSS.java

--- Arrays/NonRepeatingLongestSS.java
+++ Arrays/NonRepeatingLongestSS.java
@@ -1,11 +1,10 @@
+/**
 Question - Given a string s, find the length of the longest substring without repeating characters.
 
 Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.
-
-
-/**
+------------------------Interview practice ---------------------
 1. Repeat the question and clear the understanding.
 2. Assumptions that I can make:
     All characters are lower case.
