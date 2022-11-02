b969f92febfbe9587821831794c7ebfda1797386
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 01:25:03 -0400

    Update and rename ArraysIntersection to ArraysIntersection.java

--- Arrays/ArraysIntersection.java
+++ Arrays/ArraysIntersection.java
@@ -1,3 +1,4 @@
+/**
 Given two integer arrays nums1 and nums2, return an array of their intersection. 
 Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
@@ -10,7 +11,7 @@ Example 2:
 Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 Output: [4,9]
 Explanation: [9,4] is also accepted.
-
+*/
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         
