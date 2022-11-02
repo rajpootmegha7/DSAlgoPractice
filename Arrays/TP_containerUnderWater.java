8a67d84fd2a5739583fe13ca4268651306400b1d
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 02:55:43 -0400

    Update and rename TP_containerUnderWater to TP_containerUnderWater.java

--- Arrays/TP_containerUnderWater.java
+++ Arrays/TP_containerUnderWater.java
@@ -1,9 +1,9 @@
-
+/*
 Input: height = [1,8,6,2,5,4,8,3,7]
 Output: 49
 Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
 In this case, the max area of water (blue section) the container can contain is 49.
-
+*/
 class Solution {
     public int maxArea(int[] height) {
         
