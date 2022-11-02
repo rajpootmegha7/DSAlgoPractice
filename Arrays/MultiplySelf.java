02b491b629b310e2237c929086c59339730be73f
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 03:19:44 -0400

    Update MultiplySelf.java

--- Arrays/MultiplySelf.java
+++ Arrays/MultiplySelf.java
@@ -17,12 +17,12 @@ class Solution {
         
         int right = 1, left = 1;
         for (int i = 0; i < nums.length; i++) {
-            arr[i] = left;
+            arr[i] = left;     // simply left values are stored
             left *= nums[i];
         }
         for (int i = nums.length - 1; i >= 0; i--) {
-            arr[i] *= right;
-            right *= nums[i];
+            arr[i] *= right;   // In right we will store the multiply
+            right *= nums[i];   //Then multiply again
         }
         return arr;  
     }
