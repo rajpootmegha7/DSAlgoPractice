f26d89cea48ba2194c505d365a8cfb2190ca089c
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-02 03:24:45 -0400

    Update and rename twoSum to TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,14 +1,14 @@
 
+/**
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
 
 class Solution {
-    
-    /**
-    sol 1 : Solve it using hashmap method using two loops
-    */
-    
+  
+    //sol 1 : Solve it using hashmap method using two loops
+  
     public int[] twoSum(int[] nums, int target) {
        HashMap<Integer, Integer> prevMap = new HashMap<>();
 
