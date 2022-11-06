0caea63b3507ba783b82b7837aede41e364ddf17
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-06 18:23:34 -0500

    Update 83DuplicateRemoveLL.java

--- LinkedList/83DuplicateRemoveLL.java
+++ LinkedList/83DuplicateRemoveLL.java
@@ -1,3 +1,11 @@
+// Input: head = [1,2,3,3,4,4,5]
+// Output: [1,2,5]
+
+// Given the head of a sorted linked list, delete all nodes that have duplicate numbers, 
+// leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
