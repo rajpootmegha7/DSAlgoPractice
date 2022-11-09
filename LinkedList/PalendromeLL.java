6ae349c8bccedb9c6dc0e754845ad5e22277f932
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2022-11-09 02:15:50 -0500

    Update and rename PalendromeLL to PalendromeLL.java

--- LinkedList/PalendromeLL.java
+++ LinkedList/PalendromeLL.java
@@ -1,4 +1,14 @@
 /**
+Given the head of a singly linked list, return true if it is a 
+palindrome or false otherwise.
+
+Input: head = [1,2,2,1]
+Output: true
+
+Input: head = [1,2]
+Output: false
+
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
