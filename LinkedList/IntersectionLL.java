7dc72e75b72b17285767d50b0f543727c08d835c
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-02-08 16:05:22 -0800

    Update IntersectionLL.java

--- LinkedList/IntersectionLL.java
+++ LinkedList/IntersectionLL.java
@@ -11,13 +11,14 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-     
+        // Hashset will help us get the unique values out of the LL.
         HashSet<ListNode> ht = new HashSet<>();
-
+        //1. First iterate through list one and add all the values in the hashset.
         while(headA!=null){
             ht.add(headA);
             headA= headA.next;
         }
+        //2. First check if the values is present in the Hashset then return it and if not then move the pointer forward.
         while(headB!=null){
             if(ht.contains(headB)){
                 return headB;
