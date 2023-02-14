11b1cdbc12f6396f6e679f199b3011f33969cd64
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-02-13 21:59:26 -0800

    Create IsEasy.java

---
+++ Arrays/IsEasy.java
@@ -0,0 +1,30 @@
+/**
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+*/
+class Solution {
+
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}
