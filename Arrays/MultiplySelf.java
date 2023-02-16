7f0bcd7aa71221b056e7ce8b6d847bd2f540569c
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-02-16 00:08:32 -0800

    Update MultiplySelf.java

--- Arrays/MultiplySelf.java
+++ Arrays/MultiplySelf.java
@@ -2,6 +2,36 @@
 // Output: [24,12,8,6]
 
 
+
+//Easy Solution  I : O(n)
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        /*
+        use the prefix and sufix using two array left and right and answer
+       */
+       int[] answer = new int[nums.length];
+       int[] left = new int[nums.length];
+       int[] right = new int[nums.length];
+
+       // Store the left most prefix as 1;
+       left[0] = 1;
+       for(int i = 1; i< nums.length; i++){
+           left[i] = left[i-1] * nums[i-1]; //1*1, 1*2,2*3,6*4
+       }
+       right[nums.length -1] = 1;
+       for(int j = nums.length - 2; j >= 0; j--){
+           right[j] = right[j+1]* nums[j+1]; // 24*1,12*2,4*3,1*4 
+       }
+
+       for(int k = 0; k < nums.length; k++){
+           answer[k] = left[k]* right[k];
+       }
+        return answer;
+
+    }
+}
+
 //Just store the left and right product (Try doing this with extra space first)
 //This one is constant space because we are returning the array we created
 //In first pass calculate the left product except self and in second calculate the right
