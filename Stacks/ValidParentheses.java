8c00c21cba99718ca7bbf47b75781942d34b2691
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-02-15 21:19:56 -0800

    Update ValidParentheses.java

--- Stacks/ValidParentheses.java
+++ Stacks/ValidParentheses.java
@@ -6,22 +6,21 @@ Output: true
 **/
 
 if (s.length() % 2 != 0) return false;
+        // 1. Create a stack to keep record of characters.
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (
-                stack.isEmpty() &&
-                (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']')
-            ) return false; else {
-                if (!stack.isEmpty()) {
-                    if (
-                        stack.peek() == '(' && s.charAt(i) == ')'
-                    ) stack.pop(); else if (
-                        stack.peek() == '{' && s.charAt(i) == '}'
-                    ) stack.pop(); else if (
-                        stack.peek() == '[' && s.charAt(i) == ']'
-                    ) stack.pop(); else stack.add(s.charAt(i));
-                } else stack.add(s.charAt(i));
+                // 2. edge case where stack is empty and closig brackets are present.
+            if ( stack.isEmpty() && (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']'))
+                    return false; 
+                else {
+                       if (!stack.isEmpty()) { //4. check is stack is not empty and closing brackets are present then pop
+                            if ( stack.peek() == '(' && s.charAt(i) == ')'
+                            ) stack.pop();
+                            else if (stack.peek() == '{' && s.charAt(i) == '}' ) stack.pop(); 
+                            else if (stack.peek() == '[' && s.charAt(i) == ']' ) stack.pop(); 
+                            else stack.add(s.charAt(i)); // 5. stack is not empty and some thing else is there then add.
+                        } else stack.add(s.charAt(i)); //3. First time it will add all the brackets to the stack.
             }
         }
-        return stack.isEmpty();
+        return stack.isEmpty(); // 6. Stack is empty after everything is removed returns true.
     }
