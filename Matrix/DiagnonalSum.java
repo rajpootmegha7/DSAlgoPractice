719c897d8c936ab1e464426327a847fc9ee9a21d
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-03-11 00:04:33 -0800

    Create DiagnonalSum.java

---
+++ Matrix/DiagnonalSum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int diagonalSum(int[][] mat) { 
+      int n = mat.length;
+      int sum = 0;
+      for(int i =0; i< n ; i++){
+        sum += mat[i][i] + mat[i][n-1-i];
+      }
+      return n%2 == 1 ? sum =sum -mat[n/2][n/2] : sum;
+    }
+}
