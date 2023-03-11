2d261d89f5a52dee5c6a5944fe99be4fa5b24f2a
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-03-11 00:05:22 -0800

    Update DiagnonalSum.java

--- Matrix/DiagnonalSum.java
+++ Matrix/DiagnonalSum.java
@@ -1,3 +1,18 @@
+/** 
+Input: mat = [[1,2,3],
+              [4,5,6],
+              [7,8,9]]
+Output: 25
+Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+Notice that element mat[1][1] = 5 is counted only once.
+Example 2:
+
+Input: mat = [[1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1]]
+Output: 8
+    */
 class Solution {
     public int diagonalSum(int[][] mat) { 
       int n = mat.length;
