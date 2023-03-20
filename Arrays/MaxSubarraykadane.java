de255ed4b866e8284e81537bcc3eb408758d2f96
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-03-19 23:03:40 -0700

    Update and rename MaxSubarray.java to MaxSubarraykadane.java

--- Arrays/MaxSubarraykadane.java
+++ Arrays/MaxSubarraykadane.java
@@ -27,6 +27,22 @@ class Solution {
         return max;
 
     }
+    // kadane algo 
+    class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+        
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            max = Math.max(sum,max);
+            
+            if(sum<0) sum = 0;
+        }
+        
+        return max;
+    }
+}
   
  // Solution 2 - O(N) Kadane 's algorithm
     public int maxSubArray(int[] nums) {
