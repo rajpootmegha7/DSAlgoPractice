dead206fe718ca6f53c0ebc984154350edaca4f6
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-04-07 09:47:45 -0700

    Update InvertBinaryTree.java

--- Trees/InvertBinaryTree.java
+++ Trees/InvertBinaryTree.java
@@ -13,9 +13,13 @@ Input :
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root == null) return null;
+        // Create a new tree to save the inverted nodes in this one.
         TreeNode newroot = new TreeNode();
+        // Saving the right node in left node of new tree.
         newroot.left = invertTree(root.right);
+        // Adding the root value
         newroot.val = root.val;
+        // Adding the left nodes in the right node of the new tree.
         newroot.right = invertTree(root.left);
         return newroot;
     }
