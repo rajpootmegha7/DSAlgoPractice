4f4e6086e7086317143765462b1b606f13b40ff4
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-04-07 09:56:05 -0700

    Update Iterative InOrderTraversalTree.java

--- Trees/Iterative InOrderTraversalTree.java
+++ Trees/Iterative InOrderTraversalTree.java
@@ -12,6 +12,14 @@
  *         this.right = right;
  *     }
  * }
+  Steps to follow:
+  1. Create a new arraylist to store the sequence.
+  2. Create a stack of tree.
+  3. Loop while curr is not null or stack is not empty.
+  4. if curr root is not at end. Keep pushing in stack. Keep moving to the left.
+  5. else if end of root is reached then pop the stack and add in the list. Keep moving towards right.
+  6. Return list at the end.
+  
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
