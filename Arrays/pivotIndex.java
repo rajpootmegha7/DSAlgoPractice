a43bb7612b6507301350a37b01f6a53ef927e88f
Megha <68971156+rajpootmegha7@users.noreply.github.com>
2023-04-10 12:31:20 -0700

    Update pivotIndex.java

--- Arrays/pivotIndex.java
+++ Arrays/pivotIndex.java
@@ -17,6 +17,13 @@ The pivot index is 3.
 Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
 Right sum = nums[4] + nums[5] = 5 + 6 = 11
 
+Sol : Let's say we knew S as the sum of the numbers, and we are at index i.
+If we knew the sum of numbers leftsum that are to the left of index i, 
+then the other sum to the right of the index would just be S - nums[i] - leftsum.
+
+As such, we only need to know about leftsum to check whether an index is a pivot index in constant time. 
+Let's do that: as we iterate through candidate indexes i, we will maintain the correct value of leftsum.
+
 */
 
 class Solution {
